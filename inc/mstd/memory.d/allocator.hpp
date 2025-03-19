fd52186b6e96550722c317b9c94fe9fdee5c4149
Jack M <101893497+mopo3ob-jack@users.noreply.github.com>
2025-03-18 22:38:57 -0700

    Added Windows support for allocator.hpp

--- inc/mstd/memory.d/allocator.hpp
+++ inc/mstd/memory.d/allocator.hpp
@@ -16,7 +16,11 @@ static inline T* alloc(Size count, Size alignment = alignof(T)) {
 #ifdef MSTD_COUNT_ALLOCATIONS
 	++allocationCount;
 #endif
+#ifdef _WIN32
+	return (T*)_aligned_malloc(count * sizeof(T), alignment);
+#else
 	return (T*)std::aligned_alloc(alignment, count * sizeof(T));
+#endif
 }
 
 template <typename T>
@@ -24,14 +28,24 @@ static inline Status alloc(T*& result, Size count, Size alignment = alignof(T)) 
 #ifdef MSTD_COUNT_ALLOCATIONS
 	++allocationCount;
 #endif
+#ifdef _WIN32
+	result = (T*)_aligned_malloc(count * sizeof(T), alignment);
+#else
 	result = (T*)std::aligned_alloc(alignment, count * sizeof(T));
+#endif
 	return result != nullptr;
 }
 
 template <typename T>
-static inline Status realloc(T*& result, Size count) {
-	result = (T*)std::realloc(result, count * sizeof(T));
-	return result != nullptr;
+static inline Status realloc(T*& ptr, Size count, Size alignment = alignof(T)) {
+#ifdef _WIN32
+	ptr = _aligned_realloc((void*)ptr, count, alignment);
+#else
+	// TODO
+	// Add reallocation alignment for unix
+	ptr = (T*)std::realloc(ptr, count * sizeof(T));
+#endif
+	return ptr != nullptr;
 }
 
 template <typename T>
@@ -39,7 +53,11 @@ static inline void free(T* ptr) {
 #ifdef MSTD_COUNT_ALLOCATIONS
 	--allocationCount;
 #endif
+#ifdef _WIN32
+	_aligned_free((void*)ptr);
+#else
 	std::free((void*)ptr);
+#endif
 }
 
 }
