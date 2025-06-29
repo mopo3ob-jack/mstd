97f415d290b23e0ccb6db798a5a37b3b505305d1
Jack Morozov <jack.c.morozov@gmail.com>
2025-06-29 16:07:51 -0700

    Reworked Tree indirection

--- inc/mstd/memory.d/Tree.hpp
+++ inc/mstd/memory.d/Tree.hpp
@@ -11,41 +11,33 @@ public:
 	Tree() {}
 
 	constexpr ~Tree() {
-		if (children) {
-			for (Size i = 0; i < degree; ++i) {
-				children[i].~Tree();
+		for (Size i = 0; i < degree; ++i) {
+			if (children[i]) {
+				children[i]->~Tree();
 			}
 		}
 	}
 
-	void addChildren(Arena& arena) {
-		children = arena.reserve<Tree>(degree);
-
-		for (Size i = 0; i < degree; ++i) {
-			children[i].children = nullptr;
-		}
-	}
-
-	Tree& operator[](Size i) {
+	Tree*& operator[](Size i) {
 		return children[i];
 	}
 
-	const Tree& operator[](Size i) const {
+	const Tree*& operator[](Size i) const {
 		return children[i];
 	}
 
-	Tree* data() {
+	Tree** data() {
 		return children;
 	}
 
-	const Tree* data() const {
+	const Tree** data() const {
 		return children;
 	}
 
 	T value;
 
 private:
-	Tree* children = nullptr;
+	Tree* children[degree];
 };
 
 }
